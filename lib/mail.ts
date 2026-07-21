c15638227042c2f18d0dc2b4968bcc8064603c2a
Manx Media <sales.manxmedia@gmail.com>
2026-07-21 12:22:27 +0200

    WebsiteV15 testing the SMTP CONNECTION

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,15 +2,28 @@ import nodemailer from "nodemailer";
 import { render } from "@react-email/render";
 import React from "react";
 
+console.log("==================================");
+console.log("SMTP_HOST:", process.env.SMTP_HOST);
+console.log("SMTP_PORT:", process.env.SMTP_PORT);
+console.log("SMTP_SECURE:", process.env.SMTP_SECURE);
+console.log("SMTP_USER:", process.env.SMTP_USER);
+console.log("MAIL_FROM:", process.env.MAIL_FROM);
+console.log(
+  "SMTP_PASSWORD:",
+  process.env.SMTP_PASSWORD ? "SET" : "NOT SET"
+);
+console.log("==================================");
+
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: process.env.SMTP_SECURE === "true",
-
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
+  logger: true,
+  debug: true,
 });
 
 export async function sendEmail({
@@ -22,9 +35,11 @@ export async function sendEmail({
   subject: string;
   email: React.ReactElement;
 }) {
+  console.log(`📨 Sending email to ${to}`);
+
   const html = await render(email);
 
-  await transporter.sendMail({
+  return transporter.sendMail({
     from: process.env.MAIL_FROM,
     to,
     subject,
