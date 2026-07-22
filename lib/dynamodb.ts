ef0357921c4b7477ad618e250008d4a93b80c316
Manx Media <sales.manxmedia@gmail.com>
2026-07-22 04:44:39 +0200

    WebsiteV17DynamoCredentialsUpdate

--- lib/dynamodb.ts
+++ lib/dynamodb.ts
@@ -4,10 +4,10 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({
   region: process.env.REGION,
 
-  credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID!,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
-  },
+  //credentials: {
+    //accessKeyId: process.env.ACCESS_KEY_ID!,
+    //secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+  //},
 });
 
 export const dynamoDb = DynamoDBDocumentClient.from(client);
